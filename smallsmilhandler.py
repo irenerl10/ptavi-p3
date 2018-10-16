de473f3e6552aa9e4f93646b1521a25b503f10a7
Irene Rodriguez Lopez <irolo@alpha26.aulas.gsyc.urjc.es>
2018-10-16 12:33:07 +0200

    Programa terminado con pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,30 +5,32 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import sys
 
+
 class SmallSMILHandler(ContentHandler):
-	def __init__(self):
-		self.list = []
-		self.elements = {
-			'root-layout':['width', 'height', 'background-color'],
-			'region':['id', 'top', 'bottom', 'left', 'right'],
-			'img':['src', 'region', 'begin', 'dur'],
-			'audio':['src', 'begin', 'dur'],
-			'textstream':['src', 'region']}
+    def __init__(self):
+        self.list = []
+        self.elements = {
+            'root-layout': ['width', 'height', 'background-color'],
+            'region': ['id', 'top', 'bottom', 'left', 'right'],
+            'img': ['src', 'region', 'begin', 'dur'],
+            'audio': ['src', 'begin', 'dur'],
+            'textstream': ['src', 'region']}
 
-	def startElement(self, name, attrs):
-		if name in self.elements:
-			dicc = {}
-			dicc['element']=name
-			for atributo in self.elements[name]:
-				dicc[atributo] = attrs.get(atributo,"")
-			self.list.append(dicc)
+    def startElement(self, name, attrs):
+        if name in self.elements:
+            dicc = {}
+            dicc['element'] = name
+            for atributo in self.elements[name]:
+                dicc[atributo] = attrs.get(atributo, "")
+            self.list.append(dicc)
 
-	def get_tags(self):
-		return self.list
+    def get_tags(self):
+        return self.list
+
 
 if __name__ == "__main__":
-	parser = make_parser()
-	cHandler = SmallSMILHandler()
-	parser.setContentHandler(cHandler)
-	parser.parse(open('karaoke.py') #sys.argv[1]) para windows
-	print(cHandler.get_tags())
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    print(cHandler.get_tags())
